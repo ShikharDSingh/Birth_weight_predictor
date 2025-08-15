d956378cf715db7482c8d7c0cd09d221c05c5a11
ShikharDSingh <149926709+ShikharDSingh@users.noreply.github.com>
2025-08-15 12:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_prediction():
 
     baby_df = pd.DataFrame(baby_data_cleaned)
 
-    with open("model/model.pkl",'rb') as obj:
+    with open("model.pkl",'rb') as obj:
         model = pickle.load(obj)
     
     prediction = model.predict(baby_df)
